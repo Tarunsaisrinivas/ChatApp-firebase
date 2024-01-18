6e61e5965f361df67ebb92507cac0a2530bb873d
Tarun Sai Srinivas <tarunsaisrinivas7@gmail.com>
2024-01-18 11:49:16 +0530

    second

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -62,7 +62,7 @@ const handleDeleteMessage = async () => {
       <p className={style.name}>
         {message.name} {formattedTimestamp}
       </p>
-      <div className="invisible ">
+      <div className="visible sm:invisible ">
         <ion-icon
           name="trash-outline"
           onClick={handleDeleteMessage}
